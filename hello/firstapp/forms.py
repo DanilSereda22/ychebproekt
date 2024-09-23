8c7f2b8cfb2467ea84f612cc2774e1d0f7b7250a
Danil Sereda <seredadanil944@gmail.com>
2024-09-24 00:29:13 +0300

    23.09.2024 Sereda Danil ЛЗ9 Приобрёл общее понятие о работе с полями форм.

--- hello/firstapp/forms.py
+++ hello/firstapp/forms.py
@@ -1,5 +1,11 @@
 from django import forms
-
 class UserForm(forms.Form):
- name = forms.CharField(label="Имя клиента")
+ name = forms.CharField(label="Имя клиента",
+ widget=forms.TextInput(attrs={"class": "myfield"}))
+ age = forms.IntegerField(label="Возраст клиента",
+ widget=forms.NumberInput(attrs={"class": "myfield"}))
+
+
+
+
 
--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -1,3 +1,4 @@
+from django.http import *
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
@@ -78,6 +79,12 @@ def index(request):
 def index(request):
  userform = UserForm()
  return render(request, "firstapp/index.html", {"form": userform})
-class UserForm(forms.Form):
- file = forms.FileField(label="Файл")
 
+def index(request):
+ userform = UserForm()
+ if request.method == "POST":
+  userform = UserForm(request.POST)
+ if userform.is_valid():
+  name = userform.cleaned_data["name"]
+  return HttpResponse("<h2>Имя введено коррректно – {0}</h2>".format(name))
+ return render(request, "firstapp/index.html", {"form": userform})
