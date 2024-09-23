d9a7c8156bc7ddeac8262cd493bdd874d7b44617
Danil Sereda <seredadanil944@gmail.com>
2024-09-23 20:17:04 +0300

    23.09.2024 Sereda Danil ПЗ2 научился пользоваться параметрами представлений.

--- hello/firstapp/views.py
+++ hello/firstapp/views.py
@@ -10,4 +10,13 @@ def about(request):
 def contact(request):
  return HttpResponse("<h2>Koнтaкты</h2>")
 
+def products(request, productid):
+ output = "<h2>Продукт № {0}</h2>".format(productid)
+ return HttpResponse(output)
+
+def users(request, id=1, name="Данил"):
+ output = "<h2>Пользователь</h2><h3>id: {0} Имя: {1} ".format(id,name)
+ return HttpResponse(output)
+
+
 
--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 from firstapp import views
@@ -6,4 +7,9 @@ urlpatterns = [
  path('', views.index),
  re_path(r'^about', views.about),
  re_path(r'^contact', views.contact),
+ path('products/', views.products),
+ path('products/<int:productid>/', views.products),
+ path('users/', views.users), 
+ path('users/<int:id>/<str:name>/', views.users),
+
 ]
