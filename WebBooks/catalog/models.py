ccd09b4132615e80c1acd4ed6959441eeaa702b7
Your Name <you@example.com>
2024-11-06 11:45:41 +0300

    08.10.2024 Sereda Danil ЛЗ12 Научился пользоваться административной панелью Django

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -63,6 +63,9 @@ class Book(models.Model):
    return self.title
  def get_absolute_url(self):
    return reverse('book-detail', args=[str(self.id)])
+ def display_author(self):
+  return ', '.join([author.last_name for author in self.author.all()])
+ display_author.short_description = 'Авторы'
  
 class Status(models.Model):
  name = models.CharField(max_length=20,
